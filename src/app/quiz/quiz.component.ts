163562e82dc49a340e77fcf5ac4eff5a653365d9
MERLIER <chemli@eurecom.fr>
2020-12-19 21:25:31 +0100

    update

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -11,12 +11,15 @@ export class QuizComponent implements OnInit {
    score: number;
   colors: string[];
   selected: string[];
+  disabled: boolean;
+  answered: boolean[];
   constructor() { }
 
   // from db
   QList = ["What's the biggest animal in the world? \n  Give 3 animals How many minutes in a game of rugby league?","What does He stand for on the periodic table?","How many minutes in a game of rugby league?","Who did Anne Hathaway play in Les Miserables?"]
   QAnswers = [["They probably got my blood sample mixed up. I, uh, dropped your blood sample","They probably got my blood sample mixed up. I, uh, dropped your blood sample","They probably got my blood sample mixed up.","They probably got my blood sample mixed up. I, uh, dropped your blood sample","They probably got my blood sample mixed up."],["1","2","3","4","5"],["1","2","3","4","5"],["1","2","3","4","5"]]
-  QDecision = [[2],[2],[1],[1],[1]]
+  QDecision = [[2,3],[2],[1],[1],[1]]
+  QSelected = [[],[],[],[],[]]
   
   Scoring() {
 
@@ -24,36 +27,97 @@ export class QuizComponent implements OnInit {
     else return 0
   }
 
+Back() {
+  if (this.pointer !=0) {
+  this.pointer = this.pointer - 1
+  // if answered[pointer] block and show right answers and selected ones
+  // else initate colors and selected
+  if (this.answered[this.pointer]) {this.block(); this.showRightAnswers(); this.showSelectedAnswers()}
+  else { this.newQuestion() } }
+}
+Next() {
+  if (this.pointer + 1!=this.QAnswers.length) {
+  this.pointer = this.pointer + 1
+  if (this.answered[this.pointer]) {this.block(); this.showRightAnswers(); this.showSelectedAnswers()}
+  else { this.newQuestion() } }
+  else console.log(this.score)
+}
+Report() {
+
+}
+block(){
+  this.disabled = true;
+}
+release(){
+  this.disabled = false;
+}
+
+showRightAnswers(){
+  // delete past colors
+  for (let s of [0,1,2,3,4]) this.colors[s] = ""
+  // fill right colors
+  for ( let s of this.QDecision[this.pointer] ) { this.colors[s] = "success"; }
+}
+
+showSelectedAnswers() {
+this.selected = this.QSelected[this.pointer]
+}
+
+
+newQuestion() {
+  this.colors = ["","","","",""]
+    this.selected= ["","","","",""]
+        this.release()
+}
+
+
+  calculScore(){
+      var s =0;
+        //we assume that the length of QDecisions == QColors
+        if (this.intruderExists(this.QDecision[this.pointer],this.selected)){
+          return 0
+        }else{
+          //No intruder => score >= 0
+          for ( let i=0;i<this.QDecision[this.pointer].length;i++){
+            if( this.selected[this.QDecision[this.pointer][i]] == "blue"){
+              s =s+ 1/this.QDecision[this.pointer].length;
+            }
+          }
+        }
+    
+    console.log(s);
+    return s;
+  }
+
+  intruderExists(decision:number[],colors:string[]): boolean{
+    console.log(decision,colors)
+
+
+    for (let i=0;i<colors.length;i++){
+      if(colors[i]!="" && !decision.includes(i)){
+        return true;
+      }
+    }
+    return false;
+  }
+
 
   go() {
-    // animation
-    this.colors = ["","","","",""] 
-    for ( let s of this.QDecision[this.pointer] ) { this.colors[s] = "success"; }
-    
-    // after 1 second
-    setTimeout(() => 
-{
-  for ( let s of this.QDecision[this.pointer] ) { this.colors[s] = ""; }
-  this.score = this.score + this.Scoring()
-  
-  if(this.pointer+1 < this.QList.length)  {
-    // initialize for next question
-    this.colors = ["","","","",""]
-    this.selected= ["","","","",""]
-    this.pointer = this.pointer + 1 
-  }
-  else {
-    window.location.href = "/other/" + this.score.toString();
-  }
-},
-1500);
-    
-  
+    this.showRightAnswers()
+    this.block()
+    // answered
+    this.answered[this.pointer] = true;
+    //scoring
+    this.score = this.score + this.calculScore()
+    // saving answers
+    this.QSelected[this.pointer] = this.selected
+
   }
 
   selectAnswer(q:number) {
     if (this.selected[q] == "") this.selected[q] = "blue"
     else if (this.selected[q] == "blue") this.selected[q] = ""
+    
   }
 
   ngOnInit() {
@@ -61,6 +125,8 @@ export class QuizComponent implements OnInit {
     this.score = 0
     this.colors = ["","","","",""]
     this.selected= ["","","","",""]
+    this.answered = [false,false,false,false,false]
+    this.disabled=false
     
   }
 
