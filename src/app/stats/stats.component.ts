78cd9a6938e0ec5fdddb779de7d25980f8432ee1
ghanmi <firas.ghanmi@eurecom.fr>
2021-02-24 22:53:53 +0100

    add charts-realOnes

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -14,53 +14,99 @@ export class StatsComponent implements OnInit {
 
   showCharts() {
 
-    // Create the data table.
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Topping');
-    data.addColumn('number', 'Slices');
-    data.addRows([
-      ['Mushrooms', 3],
-      ['Onions', 1],
-      ['Olives', 1],
-      ['Zucchini', 1],
-      ['Pepperoni', 2]
+    var data = google.visualization.arrayToDataTable([
+      ['Essai', 'Temps en minutes'],
+      ['1',  17],
+      ['2',  25],
+      ['3',  30],
+      ['4',  5]
     ]);
 
-    // Set chart options
-    var options = {'title':'How Much Pizza I Ate Last Night',
-                   'width':400,
-                   'height':300};
+    var options = {
+      title: 'Title ...',
+      curveType: 'function',
+      legend: { position: 'bottom' },
+      height: 400,
+      vAxes: {
+        // Adds titles to each axis.
+        0: {title: 'Temps moyen / QCM (min)'}
+      },
+      hAxes: {
+        // Adds titles to each axis.
+        0: {title: 'Essai(s)'}
+      },
+    };
 
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
     chart.draw(data, options);
   }
 
 
   showCharts2() {
 
-    // Create the data table.
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Topping');
-    data.addColumn('number', 'Slices');
-    data.addRows([
-      ['Mushrooms', 3],
-      ['Onions', 1],
-      ['Olives', 1],
-      ['Zucchini', 1],
-      ['Pepperoni', 2]
+    var data = google.visualization.arrayToDataTable([
+      ['Essai', 'Précision'],
+      ['1',  10],
+      ['2',  40],
+      ['3',  50],
+      ['4',  95]
     ]);
 
-    // Set chart options
-    var options = {'title':'How Much Pizza I Ate Last Night',
-                   'width':400,
-                   'height':300};
+    var options = {
+      title: 'Title ...',
+      curveType: 'function',
+      legend: { position: 'bottom' },
+      height: 400,
+      vAxes: {
+        // Adds titles to each axis.
+        0: {title: 'Précision'}
+      },
+      hAxes: {
+        // Adds titles to each axis.
+        0: {title: 'Essai(s)'}
+      },
+    };
 
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart(document.getElementById('chart_divEVOLUTION'));
+    var chart = new google.visualization.LineChart(document.getElementById('chart_divEVOLUTION'));
+
     chart.draw(data, options);
   }
 
+
+
+
+  showCharts3() {
+    var data = google.visualization.arrayToDataTable([
+      // Here, axises are inverted % our logic
+      ['Nombre de QCM faits', 'QCM faits', { role: 'style' } ],
+      ['L', 10, 'color: #76A7FA'],
+      ['M', 14, 'color: #76A7FA'],
+      ['M', 16, 'color: #76A7FA'],
+      ['J', 22, 'color: #76A7FA'],
+      ['S', 28, 'stroke-color: #76A7FA'],
+      ['D', 17, 'stroke-color: #76A7FA']
+    ]);
+    
+
+    var options = {
+      
+      title: 'Title ... ',
+      legend: { position: 'none' },
+      height: 400,
+      vAxes: {
+        // Adds titles to each axis.
+        0: {title: 'Nombre de QCM faits'}
+      },
+      hAxes: {
+        // Adds titles to each axis.
+        0: {title: 'Jours'}
+      },
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('chart_moyenne_qcm_jour'));
+    chart.draw(data, options);
+  }
   
 
 }
